49832764cd214434ba4029f7e15d885ae3d92a5e
kaito02050620 <suzuki.engineer06@gmail.com>
2023-04-30 12:30:51 +0900

    修正

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -33,7 +33,7 @@ const Header = () => {
           <Link to="/" className="flex items-center button button:hover">
             <div className="lg:w-16 md:w-16 sm:w-16 w-14 md:mr-3 mr-1">
               <img
-                src="../../../public/assets/image/logo.png"
+                src="../../../../public/assets/image/logo.png"
                 alt="海の味ロゴ"
               />
             </div>
