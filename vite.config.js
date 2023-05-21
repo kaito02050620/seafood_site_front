106da90bdd84cb6eab5047a010a0caa35979fc30
kaito02050620 <suzuki.engineer@gmail.com>
2023-05-21 21:26:38 +0900

    practice_commit

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,7 @@
 import { defineConfig } from "vite";
 import FullReload from "vite-plugin-full-reload";
-// import dotenv from "dotenv";
-
-// dotenv.config();
 
 export default defineConfig({
-  plugins: [FullReload(["config/routes.rb", "app/views/**/*"])],
-  //   resolve: {
-  //     alias: {
-  //       path: "path-browserify",
-  //     },
-  //   },
-  //   define: {
-  //     "process.env": process.env,
-  //   },
+  plugins: [FullReload(["config/routes.rb", "app/views/**/*"])]
+
 });
